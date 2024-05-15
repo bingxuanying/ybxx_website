719ae16b30a9b9496a065d55c6da37e01f979c7d
bingxuanying <yingbingxuan@gmail.com>
2024-05-15 00:22:11 -0700

    Update web icon and cname

--- src/intro/IntroLeftContainer.tsx
+++ src/intro/IntroLeftContainer.tsx
@@ -1,5 +1,6 @@
 import "./IntroLeftContainer.css";
 import links from "../assets/links.json";
+import { useState } from "react";
 
 function IntroLeftContainer() {
   return (
@@ -9,28 +10,30 @@ function IntroLeftContainer() {
       </div>
       <div className="name left-container-item">Bignxuan (Ethan) Ying</div>
       <div className="left-container-item icon-bar">
-        <div
-          className="linkedin-icon"
-          onMouseUp={() => window.open(links["linkedin"], "_blank")}
-        >
-          <img className="icon " src="icons/linkedin.png" alt="linkedin-icon" />
-        </div>
-        <div
-          className="github-icon"
-          onMouseUp={() => window.open(links["github"], "_blank")}
-        >
-          <img className="icon" src="icons/github.png" alt="github-icon" />
-        </div>
-        <div
-          className="instagram-icon"
-          onMouseUp={() => window.open(links["instagram"], "_blank")}
-        >
-          <img
-            className="icon "
-            src="icons/instagram.png"
-            alt="instagram-icon"
-          />
-        </div>
+        <img
+          className="icon"
+          src="icons/linkedin.png"
+          alt="linkedin-icon"
+          onMouseUp={() => {
+            window.open(links["github"], "_blank");
+          }}
+        />
+        <img
+          className="icon"
+          src="icons/github.png"
+          alt="github-icon"
+          onMouseUp={() => {
+            window.open(links["github"], "_blank");
+          }}
+        />
+        <img
+          className="icon"
+          src="icons/instagram.png"
+          alt="instagram-icon"
+          onMouseUp={() => {
+            window.open(links["instagram"], "_blank");
+          }}
+        />
       </div>
     </div>
   );
